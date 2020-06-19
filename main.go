45bab9119cac0fc824fa08bbaafa87859b63fa77
Hiroya Hinomori <nexearth@gmail.com>
2020-06-19 23:06:34 +0900

    Add: dep

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	// "runtime"
-	// "github.com/carlescere/scheduler"
+	"runtime"
+
+	"github.com/carlescere/scheduler"
 )
 
 func main() {
-	// scheduler.Every(5).Seconds().Run(printSuccess)
-	// runtime.Goexit()
-	fmt.Println("Hello, Docker!")
+	scheduler.Every(5).Seconds().Run(printSuccess)
+	runtime.Goexit()
 }
 
 func printSuccess() {
