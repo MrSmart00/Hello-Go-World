ee2ef091390d8b55dc6ff5d3244145d474a1a708
Hiroya Hinomori <nexearth@gmail.com>
2020-06-19 23:53:13 +0900

    tweaks

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 
 	"github.com/carlescere/scheduler"
 )
 
+var count = 0
+
 func main() {
-	scheduler.Every(5).Seconds().Run(printSuccess)
+	scheduler.Every(3).Seconds().Run(printSuccess)
 	runtime.Goexit()
 }
 
 func printSuccess() {
-	fmt.Printf("success!! \n")
+	count++
+	if count > 5 {
+		fmt.Println("finish...")
+		os.Exit(0)
+	} else {
+		fmt.Println("success!!")
+	}
 }
